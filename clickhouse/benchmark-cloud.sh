be18076b4816756040025357d9669db1397b018e
Alexey Milovidov <milovidov@clickhouse.com>
2022-10-04 04:25:16 +0200

    Add first results from ClickHouse Cloud Beta

--- clickhouse/benchmark-cloud.sh
+++ clickhouse/benchmark-cloud.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Go to https://clickhouse.cloud/ and create a service.
+# To get results for various scale, go to "Actions / Advanced Scaling" and turn the slider of the minimum scale to the right.
+# The number of threads is "SELECT value FROM system.settings WHERE name = 'max_threads'".
 
 # Load the data
 
