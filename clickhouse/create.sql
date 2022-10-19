ac5f249712ed25e6e444bd87763d173919dc6f43
Nikita Taranov <nikita.taranov@clickhouse.com>
2022-10-19 16:10:42 +0000

    update ClickHouse scripts

--- clickhouse/create.sql
+++ clickhouse/create.sql
@@ -107,4 +107,5 @@ CREATE TABLE hits
     CLID INTEGER NOT NULL,
     PRIMARY KEY (CounterID, EventDate, UserID, EventTime, WatchID)
 )
-ENGINE = MergeTree;
+ENGINE = MergeTree
+SETTINGS index_granularity = 1024;
