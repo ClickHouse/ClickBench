cb7a790b247facc9266f92fa25f113da3fe4a1f6
Alexey Milovidov <milovidov@clickhouse.com>
2022-11-05 22:16:58 +0100

    Compatibility with current master

--- clickhouse-local/run.sh
+++ clickhouse-local/run.sh
@@ -8,7 +8,7 @@ cat queries.sql | while read query; do
 
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        RES=$(./clickhouse local --time --format Null --multiquery --query="$(cat create.sql); $query" 2>&1 | tail -n1)
+        RES=$(./clickhouse local --time --format Null --multiquery --progress 0 --query="$(cat create.sql); $query" 2>&1 | tail -n1)
         [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
         [[ "$i" != $TRIES ]] && echo -n ", "
 
