8b4d5af5aaf70dec477b701a228db2a22743635b
sundy-li <543950155@qq.com>
2022-11-14 18:24:04 +0800

    update results

--- databend/benchmark.sh
+++ databend/benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir databend && cd databend
-curl -LJO 'https://github.com/datafuselabs/databend/releases/download/v0.7.127-nightly/databend-v0.7.127-nightly-x86_64-unknown-linux-musl.tar.gz'
-tar xzvf 'databend-v0.7.127-nightly-x86_64-unknown-linux-musl.tar.gz'
+curl -LJO 'https://github.com/datafuselabs/databend/releases/download/v0.8.111-nightly/databend-v0.8.111-nightly-x86_64-unknown-linux-musl.tar.gz'
+tar xzvf 'databend-v0.8.111-nightly-x86_64-unknown-linux-musl.tar.gz'
  
 # databend starts with embedded meta service (meta stored in `.databend` directory, data in `_data` directory)
 ./bin/databend-query > query.log 2>&1 &
@@ -15,15 +15,12 @@ curl 'http://default@localhost:8124/' --data-binary @create.sql
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
 gzip -d hits.tsv.gz
 
-time curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: insert into hits format TSV' -H 'skip_header: 0' -H 'field_delimiter: \t' -H 'record_delimiter: \n' -F 'upload=@"./hits.tsv"'
+time curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: insert into hits format TSV' -F 'upload=@"./hits.tsv"'
 
-# {"id":"b9e20026-7eb2-4f09-a6b3-3ab79c4cb1fd","state":"SUCCESS","stats":{"rows":99997497,"bytes":81443407174},"error":null}
-# real    8m17.103s
-
-wc -l hits.tsv                                  1
-# 99997497 hits.tsv
+# {"id":"4adff699-3386-4b2e-ad65-741964166d55","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
+# real    12m14.817s
 
 du -bcs _data
-# 15376458490
+# 16075917315
 
 ./run.sh 2>&1 | tee log.txt
