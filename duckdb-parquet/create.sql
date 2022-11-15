37a698d633cc6a5e6ed53458ee3ded384070556d
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-15 15:20:32 +0100

    Handle replacement in the view creation

--- duckdb-parquet/create.sql
+++ duckdb-parquet/create.sql
@@ -1,1 +1,6 @@
-CREATE VIEW hits AS SELECT * FROM read_parquet('hits_*.parquet', binary_as_string=True);
+CREATE VIEW hits AS
+SELECT *
+	REPLACE
+	(epoch_ms(EventTime * 1000) AS EventTime,
+	 DATE '1970-01-01' + INTERVAL (EventDate) DAYS AS EventDate)
+FROM read_parquet('hits_*.parquet', binary_as_string=True);
