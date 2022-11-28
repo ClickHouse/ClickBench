8352cc33d25236a26db5445ae2a0f31a8562edbb
Patrick Lauer <patrick@gentoo.org>
2022-11-28 16:41:48 +0000

    postgresql-tuned: New directory for more agressive postgresl tuning
    Same as postgresl.
    ... except that we do parallel import now.
    
    Uses split, which is part of gnu coreutils. Limited to nproc/2 CPUs.

---
+++ postgresql-tuned/benchmark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y postgresql-common
+sudo apt-get install -y postgresql-14
+
+wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
+gzip -d hits.tsv.gz
+chmod 777 ~ hits.tsv
+
+sudo -u postgres psql -t -c 'CREATE DATABASE test'
+sudo -u postgres psql test -t < create.sql
+time split hits.tsv --verbose  -n r/$(( $(nproc)/2 ))  --filter='sudo -u postgres psql test -t -c "\\copy hits FROM STDIN WITH FREEZE"'
+
+sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
+
+# COPY 99997497
+# Time: 2341543.463 ms (39:01.543)
+
+./run.sh 2>&1 | tee log.txt
+
+sudo du -bcs /var/lib/postgresql/14/main/
+
+cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
