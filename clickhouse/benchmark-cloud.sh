69452a8a4c42254671838205e074fe89f304aaff
Alexey Milovidov <milovidov@clickhouse.com>
2023-02-12 12:46:04 +0300

    Update benchmark-cloud.sh
    Adjust to the latest version.

--- clickhouse/benchmark-cloud.sh
+++ clickhouse/benchmark-cloud.sh
@@ -13,7 +13,7 @@ clickhouse-client --host "$HOST" --password "$PASSWORD" --secure < create.sql
 
 clickhouse-client --host "$HOST" --password "$PASSWORD" --secure --query "
   INSERT INTO hits SELECT * FROM url('https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz')
-" --time --use_structure_from_insertion_table_in_table_functions 1
+" --time
 
 # 343.455
 
