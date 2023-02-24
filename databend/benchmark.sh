74a1a8ea942cba8cb4e529a4e9a937bdaaf6d122
sundy-li <543950155@qq.com>
2023-02-24 16:36:49 +0800

    update c6a.4xlarge results

--- databend/benchmark.sh
+++ databend/benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir databend && cd databend
-curl -LJO 'https://github.com/datafuselabs/databend/releases/download/v0.9.49-nightly/databend-v0.9.49-nightly-x86_64-unknown-linux-musl.tar.gz'
-tar xzvf 'databend-v0.9.49-nightly-x86_64-unknown-linux-musl.tar.gz'
+curl -LJO 'https://github.com/datafuselabs/databend/releases/download/v0.9.50-nightly/databend-v0.9.50-nightly-x86_64-unknown-linux-musl.tar.gz'
+tar xzvf 'databend-v0.9.50-nightly-x86_64-unknown-linux-musl.tar.gz'
  
 cat > config.toml << CONF
 [storage]
@@ -19,7 +19,7 @@ CONF
 ./bin/databend-query -c config.toml > query.log 2>&1 &
 
 # Load the data
-# Docs: https://databend.rs/doc/learn/analyze-hits-dataset-with-databend
+# Docs: https://databend.rs/doc/use-cases/analyze-hits-dataset-with-databend
 curl 'http://default@localhost:8124/' --data-binary @create.sql
 
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
@@ -28,13 +28,14 @@ gzip -d hits.tsv.gz
 ## Aws gp2 write performance is not stable, we must load the data when disk's write around ~500MB/s (Don't know much about the rules of gp2)
 time curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: insert into hits FILE_FORMAT = (type = TSV)' -F 'upload=@"./hits.tsv"'
 
-## in c6a.4xlarge it's ~360s, in c6a.metal it's ~70s
+## in c6a.4xlarge it's ~360s
 
-# {"id":"702fac1f-e326-4a87-a945-bc2a0d627531","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
-# real    5m59.218s
-# user    0m1.805s
-# sys     0m33.284s
+# {"id":"8c7651a3-ba62-439b-9db3-eef10fc451ad","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
+# real    5m58.652s
+# user    0m1.967s
+# sys     0m32.262s
 
+## in c6a.metal it's ~70s
 # {"id":"2792b64f-15a2-42d8-9e32-29434ad8e068","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
 # real    1m11.019s
 # user    0m0.957s
