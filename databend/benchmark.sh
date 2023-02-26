657307b68e924ec5bc1360e4182df1cd31296e44
sundy-li <543950155@qq.com>
2023-02-26 16:22:12 +0800

    update results to v0.9.53 for c6a.4xlarge

--- databend/benchmark.sh
+++ databend/benchmark.sh
@@ -27,12 +27,17 @@ gzip -d hits.tsv.gz
 ## Aws gp2 write performance is not stable, we must load the data when disk's write around ~500MB/s (Don't know much about the rules of gp2)
 time curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: insert into hits FILE_FORMAT = (type = TSV)' -F 'upload=@"./hits.tsv"'
 
-## in c6a.4xlarge it's ~360s
+## in c5.4x large, it's 368s
+# {"id":"17477ed9-9f1a-46d9-b6cf-12a5971f4450","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
+# real    6m8.975s
+# user    0m4.327s
+# sys     0m36.185s
 
-# {"id":"8c7651a3-ba62-439b-9db3-eef10fc451ad","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
-# real    5m58.652s
-# user    0m1.967s
-# sys     0m32.262s
+## in c6a.4xlarge it's ~360s
+# {"id":"f7506581-a4da-4684-850c-4bd03530314d","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
+# real    5m57.800s
+# user    0m2.106s
+# sys     0m33.507s
 
 ## in c6a.metal it's ~70s
 # {"id":"2564bd91-1b36-4cf2-a95e-de46c5aff0c6","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
@@ -40,11 +45,6 @@ time curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: 
 # user    0m0.953s
 # sys     0m20.401s
 
-## in c5.4x large, it's 368s
-# {"id":"17477ed9-9f1a-46d9-b6cf-12a5971f4450","state":"SUCCESS","stats":{"rows":99997497,"bytes":74807831229},"error":null,"files":["hits.tsv"]}
-# real    6m8.975s
-# user    0m4.327s
-# sys     0m36.185s
 
 
 ## check data is correct
@@ -53,5 +53,9 @@ curl 'http://default@localhost:8124/' --data-binary "select count() from hits"
 du -bcs _data
 # 20922561953     _data
 # 20922561953     total
+
+# If you wants to get the data size(without metadata and indexes)
+# curl 'http://default@localhost:8124/' --data-binary "select humanize_size(bytes_compressed)  from fuse_snapshot('default', 'hits') select humanize_size(bytes_compressed)  from fuse_snapshot('default', 'hits') order by timestamp desc limit 1"
+# 18.48 GiB
 
 ./run.sh 2>&1 | tee log.txt
