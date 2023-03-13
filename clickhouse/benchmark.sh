e8621989d6f19a9eda774a44340fb7569487f3fa
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-03-13 20:00:17 +0000

    stash

--- clickhouse/benchmark.sh
+++ clickhouse/benchmark.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+set -x
+
+while getopts "tz" arg
+do
+  case $arg in
+    t)
+      tuned=1
+      ;;
+    z)
+      use_zstd=1
+      ;;
+  esac
+done
+
 # Install
 
-curl https://clickhouse.com/ | sh
-sudo DEBIAN_FRONTEND=noninteractive ./clickhouse install
+#curl https://clickhouse.com/ | sh
+#sudo DEBIAN_FRONTEND=noninteractive ./clickhouse install
+
+# Clean up non-default config files:
+sudo rm /etc/clickhouse-server/config.d/compression.yaml
+sudo rm /etc/clickhouse-server/users.d/custom-settings.yaml
 
 # Optional: if you want to use higher compression:
-if (( 0 )); then
+if (( use_zstd )); then
     echo "
 compression:
     case:
@@ -18,15 +36,32 @@ sudo clickhouse start
 
 # Load the data
 
-clickhouse-client < create.sql
+cpus=$(lscpu -J | jq '.lscpu[] | select(.field=="CPU(s):") | .data' | bc)
 
-wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
-gzip -d hits.tsv.gz
+if (( ! tuned )); then
+    clickhouse-client < create.sql
+else
+    echo "
+profiles:
+    default:
+        allow_aggregate_partitions_independently: true
+        force_aggregate_partitions_independently: true
+    " | sudo tee /etc/clickhouse-server/users.d/custom-settings.yaml
+
+    cp create-tuned.tmpl create-tuned.sql
+    sed -i "s/MAX_THREADS/$cpus/g" create-tuned.sql
+    clickhouse-client < create-tuned.sql
+    rm create-tuned.sql
+fi;
+
+#wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
+#gzip -d hits.tsv.gz
 
 clickhouse-client --time --query "INSERT INTO hits FORMAT TSV" < hits.tsv
+clickhouse-client --time --query "OPTIMIZE TABLE hits FINAL"
 
 # Run the queries
 
-./run.sh
+./run.sh $tuned
 
 clickhouse-client --query "SELECT total_bytes FROM system.tables WHERE name = 'hits' AND database = 'default'"
