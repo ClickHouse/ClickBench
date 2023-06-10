b4c3f960f2c3afd90b724787dc37d1750b7b237b
Alexey Milovidov <milovidov@clickhouse.com>
2023-06-10 12:00:15 +0200

    Rename stateless engines over Parquet for clarity

--- clickhouse-parquet/benchmark.sh
+++ clickhouse-parquet/benchmark.sh
@@ -4,15 +4,15 @@
 
 curl https://clickhouse.com/ | sh
 
-# Use for clickhouse-local (single)
+# Use for ClickHouse (Parquet, single)
 # wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.parquet'
 
-# Use for clickhouse-local (partitioned)
+# Use for ClickHouse (Parquet, partitioned)
 seq 0 99 | xargs -P100 -I{} bash -c 'wget --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
 
 # Run the queries
 
 ./run.sh
 
-# Use for clickhouse-local (single)
+# Use for ClickHouse (Parquet, single)
 # du -b hits.parquet
