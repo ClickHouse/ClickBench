3f0fbf663faa7537ecf5f0fd8de384728686b81f
DoubleMindy <doublemind21@gmail.com>
2024-02-21 15:09:14 +0300

    Fixed issues with JAVA_HOME, ssh connection and correct HDFS scripts execution

--- spark/benchmark.sh
+++ spark/benchmark.sh
@@ -5,23 +5,28 @@ sudo apt-get -y install openjdk-8-jdk-headless
 
 ### If there is no HDFS and Spark on your system:
 
-# wget --continue https://downloads.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
-# wget --continue https://downloads.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz
-# tar -xzf hadoop-3*
-# tar -xzf spark-3*
-# sudo mkdir -p /urs/local/hadoop
-# sudo mkdir -p /usr/local/spark
-# sudo mv hadoop-3* /usr/local/hadoop
-# sudo mv spark-3* /usr/local/spark
+export HADOOP_DIR="/usr/local/hadoop"
+export SPARK_DIR="/usr/local/spark"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
-# echo "export HADOOP_HOME=/usr/local/hadoop" >> ~/.bashrc
-# echo "export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin" >> ~/.bashrc
-# echo "export SPARK_HOME=/usr/local/spark>" >> ~/.bashrc
-# echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
+# wget --continue https://downloads.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
+# wget --continue https://downloads.apache.org/spark/spark-3.5.0/spark-3.5.0.tgz
+# tar -xzf hadoop-3*.tar.gz
+# tar -xzf spark-3*.tgz
+# sudo rm -f spark-3*.tgz hadoop-3*.tar.gz
+# sudo mv spark-3* spark
+# sudo mv hadoop-3* hadoop
+# sudo mv spark $HADOOP_DIR
+# sudo mv hadoop $SPARK_DIR
+
+echo "export HADOOP_HOME=$HADOOP_DIR" >> ~/.bashrc
+echo "export SPARK_HOME=$SPARK_DIR" >> ~/.bashrc
+echo "export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH" >> ~/.bashrc
+# source ~/.bashrc
 
 ### To configure HDFS:
 
-#cd /usr/local/hadoop/etc/hadoop
+#cd $HADOOP_HOME/etc/hadoop
 #cp core-site.xml core-site.xml.bak
 #cp hdfs-site.xml hdfs-site.xml.bak
 # echo "<configuration>
@@ -36,23 +41,29 @@ sudo apt-get -y install openjdk-8-jdk-headless
 #     <value>1</value>
 #   </property>
 # </configuration>" > hdfs-site.xml
+# echo "export JAVA_HOME=$JAVA_HOME" >> hadoop-env.sh
 
 ### To configure Spark:
 
-# cd /usr/local/spark/conf
+# cd $SPARK_HOME/conf
 # cp spark-env.sh.template spark-env.sh
-# echo "export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop" >> spark-env.sh
+# echo "export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop" >> spark-env.sh
 # echo "export SPARK_MASTER_HOST=localhost" >> spark-env.sh
+
+### To run passless connection on localhost:
+
+# ssh-keygen -t rsa
+# cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+# chmod og-wx ~/.ssh/authorized_keys 
 
 ### To run Spark and HDFS:
 
-# start-master.sh
-# start-slave.sh spark://localhost:7077
+# cd $SPARK_HOME/sbin
+# ./start-master.sh
+# ./start-slave.sh spark://localhost:7077
+# cd $HADOOP_HOME/sbin
 # hdfs namenode -format
-# start-dfs.sh
-
-
-source ~/.bashrc
+# ./start-dfs.sh
 
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
 gzip -d hits.tsv.gz
