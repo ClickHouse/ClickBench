3f0eaccc865f9726fad76c1da8776db4586c221b
joocer <justin.joyce@joocer.com>
2024-05-13 19:43:58 +0100

    venv

--- opteryx/benchmark.sh
+++ opteryx/benchmark.sh
@@ -5,14 +5,24 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust-init.sh
 bash rust-init.sh -y
 source ~/.cargo/env
 
+# Update package lists
 apt-get update
-apt-get install -y python3-pip
+
+# Install required packages
+apt-get install -y python3-full python3-venv git wget
+
+# Create and activate a virtual environment
+python3 -m venv ~/opteryx_venv
+source ~/opteryx_venv/bin/activate
+
+# Install pip in the virtual environment
+~/opteryx_venv/bin/pip install --upgrade pip
 
 # Install Opteryx main branch
 git clone https://github.com/mabel-dev/opteryx.git
 cd opteryx
-python3 -m pip install --upgrade -r requirements.txt
-python3 -m pip install --upgrade -r tests/requirements.txt
+~/opteryx_venv/bin/pip install --upgrade -r requirements.txt
+~/opteryx_venv/bin/pip install --upgrade -r tests/requirements.txt
 make compile
 
 # Download benchmark target data, partitioned
@@ -23,3 +33,5 @@ seq 0 99 | xargs -P100 -I{} bash -c 'wget --no-verbose --directory-prefix hits -
 cd ..
 ./run.sh
 
+# Deactivate the virtual environment
+deactivate
