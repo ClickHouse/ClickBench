e0b74d2b1990dc181d8ab30bb3d95bccf5f0f4c8
Ming Ying <ming.ying.nyc@gmail.com>
2024-05-31 15:02:33 -0400

    remove create_s3 and make run.sh use same psql connection for hot run

--- paradedb/run.sh
+++ paradedb/run.sh
@@ -8,7 +8,6 @@ cat queries.sql | while read query; do
     echo 3 | sudo tee /proc/sys/vm/drop_caches
 
     echo "$query";
-    for i in $(seq 1 $TRIES); do
-    psql -h localhost -U postgres -d mydb -p 5432 -t -c '\timing' -c "$query" | grep 'Time'
-    done;
+    output=$(psql -h localhost -U postgres -d mydb -p 5432 -t -c '\timing' -c "$query" -c "$query" -c "$query")
+    echo "$output" | grep 'Time'
 done;
