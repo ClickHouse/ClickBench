ec75786f65fb09fa08984ddffcd953f9e0a89477
auxten <auxtenwpc@gmail.com>
2024-08-12 18:10:15 +0800

    Add process log.txt

--- chdb-parquet/benchmark.sh
+++ chdb-parquet/benchmark.sh
@@ -12,3 +12,7 @@ seq 0 99 | xargs -P100 -I{} bash -c 'wget --no-verbose --continue https://datase
 # Run the queries
 
 ./run.sh 2>&1 | tee log.txt
+
+# Process the log.txt
+cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
