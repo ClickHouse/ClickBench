b1a4bd14be5386b798de352c0c02a8bec003fcdc
Alexey Milovidov <milovidov@clickhouse.com>
2024-11-07 16:45:07 +0000

    Update ClickBench for the latest master

---
+++ clickhouse-web/update-from-play.sql
@@ -0,0 +1,51 @@
+#!/usr/bin/clickhouse-client --queries-file
+
+WITH
+    extractGroups(content, '\nhttps://raw\.githubusercontent\.com/ClickHouse/ClickBench/main/([\w\-]+)/\n(\w+\.(?:\dxlarge|metal))') AS suite_machine,
+    extractGroups(content, '\n([\d\.]+)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)(\d+)') AS time_results_size,
+    extractAll(content, '(?:Single:|Partitioned:)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)') AS stateless_results
+SELECT format(
+$${{
+    "system": "ClickHouse (web)",
+    "date": "{}",
+    "machine": "c5n.4xlarge, 500gb gp2",
+    "cluster_size": 1,
+    "comment": "",
+    "tags": ["C++", "column-oriented", "ClickHouse derivative", "serverless", "stateless"],
+    "load_time": {},
+    "data_size": {},
+    "result": [
+{}
+    ]
+}}
+$$, time::Date, time_results_size[1], time_results_size[3], replaceRegexpOne(time_results_size[2], ',\n$', '')) AS res
+FROM sink.data
+WHERE suite_machine[1] = 'clickhouse-web' AND suite_machine[2] = 'c5n.4xlarge'
+ORDER BY time DESC LIMIT 1
+INTO OUTFILE 'results/c5n.4xlarge.json' TRUNCATE
+FORMAT Raw;
+
+WITH
+    extractGroups(content, '\nhttps://raw\.githubusercontent\.com/ClickHouse/ClickBench/main/([\w\-]+)/\n(\w+\.(?:\dxlarge|metal))') AS suite_machine,
+    extractGroups(content, '\n([\d\.]+)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)(\d+)') AS time_results_size,
+    extractAll(content, '(?:Single:|Partitioned:)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)') AS stateless_results
+SELECT format(
+$${{
+    "system": "ClickHouse (web)",
+    "date": "{}",
+    "machine": "c6a.metal, 500gb gp2",
+    "cluster_size": 1,
+    "comment": "",
+    "tags": ["C++", "column-oriented", "ClickHouse derivative", "serverless", "stateless"],
+    "load_time": {},
+    "data_size": {},
+    "result": [
+{}
+    ]
+}}
+$$, time::Date, time_results_size[1], time_results_size[3], replaceRegexpOne(time_results_size[2], ',\n$', '')) AS res
+FROM sink.data
+WHERE suite_machine[1] = 'clickhouse-web' AND suite_machine[2] = 'c6a.metal'
+ORDER BY time DESC LIMIT 1
+INTO OUTFILE 'results/c6a.metal.json' TRUNCATE
+FORMAT Raw;
