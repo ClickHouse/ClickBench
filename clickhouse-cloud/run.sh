03f75e3acaa1583f4188430265d7e11c19afa2a7
Alexey Milovidov <milovidov@clickhouse.com>
2024-11-12 16:43:56 +0100

    Maybe better

--- clickhouse-cloud/run.sh
+++ clickhouse-cloud/run.sh
@@ -5,8 +5,8 @@ QUERY_NUM=1
 cat queries.sql | while read query; do
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        RES=$(clickhouse-client --host "${FQDN:=localhost}" --password "${PASSWORD:=}" ${PASSWORD:+--secure} --time --format=Null --query="$query" --progress 0 ||:)
-        [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
+        clickhouse-client --host "${FQDN:=localhost}" --password "${PASSWORD:=}" ${PASSWORD:+--secure} --time --format=Null --query="$query" --progress 0 2>&1 |
+            grep -P '^\d+\.\d+$' || echo -n "null"
         [[ "$i" != $TRIES ]] && echo -n ", "
     done
     echo "],"
