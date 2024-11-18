efeaae925b4c6fcde00260290fbb4af8de4b4c1c
Philip Dubé <philip@peerdb.io>
2024-11-18 18:29:50 +0000

    results

--- pg_duckdb/benchmark.sh
+++ pg_duckdb/benchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo apt-get install -y postgresql-client
+#sudo apt-get update
+#sudo apt-get install -y docker.io
+#sudo apt-get install -y postgresql-client
 
 wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena/hits.parquet
 sudo docker run -d --name pgduck -p 5432:5432 -e POSTGRES_PASSWORD=duckdb -v ./hits.parquet:/tmp/hits.parquet pgduckdb/pgduckdb:16-main
@@ -13,7 +13,7 @@ sleep 5
 psql postgres://postgres:duckdb@localhost:5432/postgres -f create.sql
 ./run.sh 2>&1 | tee log.txt
 
-sudo docker exec -it pgduck du -bcs /var/lib/postgresql/data
+sudo docker exec -it pgduck du -bcs /var/lib/postgresql/data /tmp/hits.parquet
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
     awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
