bfff3fd67c4a775dda5859949f2e87d7a31c6ad4
Jordan Tigani <jordan@motherduck.com>
2024-11-24 18:01:53 -0800

    Updated pgduckdb motherduck scripts to not use dual with or without motherduck enabled for simplicity sake.

--- pgduckdb-motherduck/load.sh
+++ pgduckdb-motherduck/load.sh
@@ -3,15 +3,8 @@
 CONNECTION=postgres://postgres:duckdb@172.17.0.2:5432/postgres
 PSQL=psql
 
-USE_MOTHERDUCK=1
-
-if [[ $USE_MOTHERDUCK == 1 ]]; then
 DATABASE='ddb$pgclick'
 PARQUET_FILE='https:\/\/datasets.clickhouse.com\/hits_compatible\/hits.parquet'
-else
-DATABASE=temp
-PARQUET_FILE='\/tmp\/hits.parquet'
-fi
 
 echo "Loading data"
 (
--- pgduckdb-motherduck/run.sh
+++ pgduckdb-motherduck/run.sh
@@ -2,18 +2,13 @@
 
 TRIES=3
 CONNECTION=postgres://postgres:duckdb@172.17.0.2:5432/postgres
-USE_MOTHERDUCK=1
 
-if [[ $USE_MOTHERDUCK == 1 ]]; then
 DATABASE='ddb$pgclick'
-else
-DATABASE=temp
-fi
 
 cat queries.sql | while read query; do
     echo "$query"
     (
-	echo "set search_path=$DATABASE;"
+	    echo "set search_path=$DATABASE;"
         echo '\timing'
         yes "$query" | head -n $TRIES
     ) | psql $CONNECTION | grep 'Time'
