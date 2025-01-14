4c72a98c92e63159bb1a345fa561e84a9a1c6dd4
Zhou Sun <zhou@mooncakelabs.com>
2025-01-14 08:43:33 -0800

    comments

--- pg_mooncake/benchmark.sh
+++ pg_mooncake/benchmark.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo apt-get install -y postgresql-client
+#install docker/ postgres if needed.
+
+# sudo apt-get update
+# sudo apt-get install -y docker.io
+# sudo apt-get install -y postgresql-client
+# sudo usermod -aG docker $USER
+# newgrp docker
 
 wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena/hits.parquet
-sudo docker run -d --name pg_mooncake -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v ./hits.parquet:/tmp/hits.parquet mooncakelabs/pg_mooncake:17-v0.1.0
+docker run -d --name pg_mooncake -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -v ./hits.parquet:/tmp/hits.parquet mooncakelabs/pg_mooncake:17-v0.1.0
 
 sleep 5
 psql postgres://postgres:pg_mooncake@localhost:5432/postgres -f create.sql
@@ -16,7 +20,7 @@ psql postgres://postgres:pg_mooncake@localhost:5432/postgres -f create.sql
 
 ./run.sh 2>&1 | tee log.txt
 
-sudo docker exec -it pg_mooncake du -bcs /var/lib/postgresql/data
+docker exec -it pg_mooncake du -bcs /var/lib/postgresql/data
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
     awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
