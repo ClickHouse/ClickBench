092bdc8ac3c7ca75cbdf5fcc738c7ebcdfc5ae89
adam-szymanski <adam@oxla.net>
2025-02-07 18:27:58 +0100

    Applied comments regarding benchmark setup.

--- oxla/benchmark.sh
+++ oxla/benchmark.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 
 # docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce
+sudo apt install docker.io
 
 # base
 sudo apt-get install -y postgresql-client curl wget apt-transport-https ca-certificates software-properties-common gnupg2 parallel
@@ -15,14 +12,13 @@ echo "Download dataset."
 wget --no-verbose --continue 'https://datasets.clickhouse.com/hits_compatible/hits.csv.gz'
 echo "Unpack dataset."
 gzip -d hits.csv.gz
-chmod 777 ~ hits.csv
-mkdir ~/data
-mv hits.csv ~/data
+mkdir data
+mv hits.csv data
 
 # get and configure Oxla image
 echo "Install and run Oxla."
 
-sudo docker run --rm -p 5432:5432 -v ~/data:/data --name oxlacontainer public.ecr.aws/oxla/release:1.53.0-beta > /dev/null 2>&1 &
+sudo docker run --rm -p 5432:5432 -v data:/data --name oxlacontainer public.ecr.aws/oxla/release:1.53.0-beta > /dev/null 2>&1 &
 sleep 30 # waiting for container start and db initialisation (leader election, etc.)
 
 # create table and ingest data
