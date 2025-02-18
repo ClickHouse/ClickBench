31e1f49d3e19193e8299f1a9ac0e2ef95922be01
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-18 14:14:11 +0100

    pg_duckdb: Add set -x to pg_duckdb-motherduck

--- pg_duckdb-motherduck/load.sh
+++ pg_duckdb-motherduck/load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
 PSQL=psql
 
@@ -8,8 +10,7 @@ PARQUET_FILE='https:\/\/datasets.clickhouse.com\/hits_compatible\/hits.parquet'
 
 echo "Loading data"
 (
-	echo "\timing"
-	cat create.sql | 
-		sed -e "s/REPLACE_SCHEMA/$DATABASE/g" -e "s/REPLACE_PARQUET_FILE/$PARQUET_FILE/g" 
+    echo "\timing"
+    cat create.sql |
+        sed -e "s/REPLACE_SCHEMA/$DATABASE/g" -e "s/REPLACE_PARQUET_FILE/$PARQUET_FILE/g"
 ) | $PSQL $CONNECTION | grep 'Time'
-
