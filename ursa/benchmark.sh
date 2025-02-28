f0bea854cf72db710637c1a16982c3800d054aa2
Maksim Kita <kitaetoya@gmail.com>
2025-02-28 14:54:57 +0300

    Update S3 binary url

--- ursa/benchmark.sh
+++ ursa/benchmark.sh
@@ -2,7 +2,7 @@
 
 # Install
 
-wget "https://ursa-private-builds.s3.eu-central-1.amazonaws.com/ursa"
+wget "https://ursa-private-builds.s3.eu-central-1.amazonaws.com/ursa-0.0.1/ursa"
 chmod +x ursa
 
 ./ursa server &
