3a05b2d7fc6857bb0577aa9daeb3387587dbe633
Robert Schulze <robert@clickhouse.com>
2025-03-05 08:47:32 +0000

    Add results

--- pg_duckdb/run.sh
+++ pg_duckdb/run.sh
@@ -6,8 +6,8 @@ cat queries.sql | while read -r query; do
     sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-    echo "$query";
+    echo "$query"
     for i in $(seq 1 $TRIES); do
-        psql postgres://postgres:duckdb@localhost:5432/postgres -t -c '\timing' -c "$query" | grep 'Time'
-    done;
-done;
+        echo -e "\\\timing\n$query" | docker exec -i pgduck psql -U postgres -d test -t | grep 'Time'
+    done
+done
