72b048ffdd0bcb006eaded81fc6dc13453254bd3
Ubuntu <ubuntu@ip-172-30-1-175.ec2.internal>
2025-03-06 18:34:00 +0000

    Updating Kinetica benchmark for 7.2.2.11

---
+++ kinetica/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Install java for kisql and rg for run.sh
+sudo apt install openjdk-21-jre-headless rg
+
