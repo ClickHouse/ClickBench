b892e3c6e2629e76e4d7febd26cae3c094bfab28
Jelte Fennema-Nio <jelte@motherduck.com>
2025-03-07 21:54:40 +0100

    pg_duckdb: Use -r for motherduck

--- pg_duckdb-motherduck/run.sh
+++ pg_duckdb-motherduck/run.sh
@@ -5,10 +5,10 @@ CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
 
 DATABASE='ddb$pgclick'
 
-cat queries.sql | while read query; do
+cat queries.sql | while read -r query; do
     echo "$query"
     (
-	    echo "set search_path=$DATABASE;"
+        echo "set search_path=$DATABASE;"
         echo '\timing'
         yes "$query" | head -n $TRIES
     ) | psql $CONNECTION | grep 'Time'
