6fae1a911051c4055ccd427596ca977194dcae00
Jelte Fennema-Nio <jelte@motherduck.com>
2025-03-07 21:54:55 +0100

    pg_duckdb: Change sed command to not require escaping

--- pg_duckdb-motherduck/load.sh
+++ pg_duckdb-motherduck/load.sh
@@ -6,11 +6,11 @@ CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
 PSQL=psql
 
 DATABASE='ddb$pgclick'
-PARQUET_FILE='https:\/\/datasets.clickhouse.com\/hits_compatible\/hits.parquet'
+PARQUET_FILE='https://datasets.clickhouse.com/hits_compatible/hits.parquet'
 
 echo "Loading data"
 (
     echo "\timing"
     cat create.sql |
-        sed -e "s/REPLACE_SCHEMA/$DATABASE/g" -e "s/REPLACE_PARQUET_FILE/$PARQUET_FILE/g"
+        sed -e "s=REPLACE_SCHEMA=$DATABASE=g" -e "s=REPLACE_PARQUET_FILE=$PARQUET_FILE=g"
 ) | $PSQL $CONNECTION | grep 'Time'
