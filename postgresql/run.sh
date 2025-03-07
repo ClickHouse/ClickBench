477db912a0216e2013707a0d0bd8018462a2c1f8
Jelte Fennema-Nio <jelte@motherduck.com>
2025-03-07 23:24:00 +0100

    postgresql: Use the same connection for postgres for each query

--- postgresql/run.sh
+++ postgresql/run.sh
@@ -6,8 +6,9 @@ cat queries.sql | while read -r query; do
     sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-    echo "$query";
-    for i in $(seq 1 $TRIES); do
-        sudo -u postgres psql test -t -c '\timing' -c "$query" | grep 'Time'
-    done;
-done;
+    echo "$query"
+    (
+        echo '\timing'
+        yes "$query" | head -n $TRIES
+    ) | sudo -u postgres psql test -t | grep 'Time'
+done
