3bc0b6de8ce4f307ef2dacd0bc50091c251ef719
Jelte Fennema-Nio <jelte@motherduck.com>
2025-03-07 23:24:02 +0100

    postgresql: Move data loading to script for easier timing

--- postgresql/benchmark.sh
+++ postgresql/benchmark.sh
@@ -18,18 +18,7 @@ gzip -d hits.tsv.gz
 sudo -u postgres psql -t -c 'CREATE DATABASE test'
 sudo -u postgres psql test -t < create.sql
 
-# Load data: wrap TRUNCATE and \copy FREEZE in a single transaction
-# If we dont' do this, Postgres will throw an error:
-#     "ERROR: cannot perform COPY FREEZE because the table was not created or truncated in the current subtransaction"
-# (i.e. Postgres requires that the table be either created or truncated in the current subtransaction)
-sudo -u postgres psql test <<'EOF'
-BEGIN;
-TRUNCATE TABLE hits;
-\copy hits FROM 'hits.tsv' with freeze;
-COMMIT;
-EOF
-
-sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
+time ./load.sh
 
 # COPY 99997497
 # Time: 2341543.463 ms (39:01.543)
