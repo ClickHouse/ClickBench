144a17429e2ffbeee517f86aa83e19ac6c7ff500
Alexey Dmitriev <slonnn@yandex-team.ru>
2025-03-28 10:11:17 +0000

    minor fixes

--- ydb/run.sh
+++ ydb/run.sh
@@ -7,13 +7,17 @@ YDB_PASSWORD=password
 
 cert_dir=$(find ydb-ansible-examples/TLS/CA/certs -maxdepth 1 -type d -not -path "." -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f2-)
 
+# YDB uses raw block devices, that means there is not need to drop filesystem caches
+# sync
+# echo 3 | sudo tee /proc/sys/vm/drop_caches
+
 cat queries.sql | while read -r query; do
     echo -n "["
 
     for i in $(seq 1 $TRIES); do
         result=$(echo $YDB_PASSWORD | ydb-ansible-examples/3-nodes-mirror-3-dc/files/ydb -e grpcs://$host1$host_suffix:2135 -d /Root/database --ca-file $cert_dir/ca.crt --user root yql -s "$query" --stats basic 2>/dev/null)
 
-    # Extracting total_duration_us value
+        # Extracting total_duration_us value
         if [[ "$result" =~ total_duration_us:[[:space:]]*([0-9]+) ]]; then
             duration_us=${BASH_REMATCH[1]}
             # Convert microseconds to seconds
