2e0bd8a47c523b57caa26a706e3fee70b190a6b7
Robert Schulze <robert@clickhouse.com>
2025-03-29 16:18:11 +0000

    Update results

--- datafusion/benchmark.sh
+++ datafusion/benchmark.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Install Rust
+echo "Install Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust-init.sh
 bash rust-init.sh -y
 source ~/.cargo/env
 
+echo "Install Dependencies"
+sudo apt-get update
+sudo apt-get install --yes gcc
 
-# Install Dependencies
-sudo yum update -y
-sudo yum install gcc -y
-
-
-# Install DataFusion main branch
+echo "Install DataFusion main branch"
 git clone https://github.com/apache/arrow-datafusion.git
 cd arrow-datafusion/datafusion-cli
 git checkout 45.0.0
@@ -19,15 +17,13 @@ CARGO_PROFILE_RELEASE_LTO=true RUSTFLAGS="-C codegen-units=1" cargo build --rele
 export PATH="`pwd`/target/release:$PATH"
 cd ../..
 
-
-# Download benchmark target data, single file
+echo "Download benchmark target data, single file"
 wget --continue https://datasets.clickhouse.com/hits_compatible/hits.parquet
 
-# Download benchmark target data, partitioned
+echo "Download benchmark target data, partitioned"
 mkdir -p partitioned
 seq 0 99 | xargs -P100 -I{} bash -c 'wget --directory-prefix partitioned --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
 
-# Run benchmarks for single parquet and partitioned
+echo "Run benchmarks for single parquet and partitioned"
 ./run.sh single
 ./run.sh partitioned
-
