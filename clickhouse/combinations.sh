1781b071e07960b91b4586fbe8dae4563ed0e3a4
Alexey Milovidov <milovidov@clickhouse.com>
2025-04-09 00:10:44 +0000

    Maybe better

--- clickhouse/combinations.sh
+++ clickhouse/combinations.sh
@@ -8,7 +8,7 @@ do
     pushd "${BENCHMARK}"
     for MACHINE in c6a.metal c6a.4xlarge
     do
-        aws ec2 run-instances --count 1 --image-id ami-0d1ddd83282187d18 --instance-type "${MACHINE}" --block-device-mappings 'DeviceName=/dev/sda1,Ebs={DeleteOnTermination=true,VolumeSize=500,VolumeType=gp2}' --key-name milovidov --security-group-ids sg-013790293e9640422 --instance-initiated-shutdown-behavior terminate --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=clickbench}]' --user-data file://cloud-init.sh
+        aws ec2 run-instances --count 1 --image-id ami-0d1ddd83282187d18 --instance-type "${MACHINE}" --block-device-mappings 'DeviceName=/dev/sda1,Ebs={DeleteOnTermination=true,VolumeSize=500,VolumeType=gp2}' --key-name milovidov --security-group-ids sg-013790293e9640422 --instance-initiated-shutdown-behavior terminate --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=clickbench-${BENCHMARK}-${MACHINE}}]" --user-data file://cloud-init.sh
     done
     popd
 done
