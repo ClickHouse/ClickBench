9456e8cbb75f39380f1f99c96e343f1aead9635f
Ubuntu <caetanosauer@gmail.com>
2025-05-03 12:45:34 +0000

    Update Parquet results for Hyper
    This fixes an issue with how we were handling text columns that are
    stored as byte arrays in Parquet. With a new option `binary_as_text`,
    these columns are correctly interpreted as text rather than binary,
    and the query results are now correct.
    
    Once again kudos to Mark Raasveldt who reported this months ago.

--- hyper-parquet/benchmark.sh
+++ hyper-parquet/benchmark.sh
@@ -2,9 +2,17 @@
 
 sudo apt-get update
 sudo apt-get install -y python3-pip
-pip install --break-system-packages tableauhyperapi
 
-seq 0 99 | xargs -P100 -I{} bash -c 'wget --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
+PIP_MAJOR=$(echo $(pip --version | awk '{print $2}') | cut -d. -f1)
+if [ $PIP_MAJOR -ge 23 ]; then
+    pip install --break-system-packages tableauhyperapi
+else
+    pip install tableauhyperapi
+fi
+
+if [ ! -f hits_0.parquet ]; then
+    seq 0 99 | xargs -P100 -I{} bash -c 'wget --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
+fi
 
 ./run.sh | tee log.txt
 
