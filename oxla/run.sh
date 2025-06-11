2ed7f5d7a29578850100cce67abed164d60d94dc
Robert Schulze <robert@clickhouse.com>
2025-06-11 16:08:26 +0000

    Cleanup Oxla runner and re-run benchmark

--- oxla/run.sh
+++ oxla/run.sh
@@ -4,15 +4,15 @@ TRIES=3
 rm result.txt 2>/dev/null
 cat queries.sql | while read -r query; do
     sync
-    echo 3 | sudo tee /proc/sys/vm/drop_caches 1>/dev/null
+    echo 3 | sudo tee /proc/sys/vm/drop_caches
 
     echo "$query";
     results=""
     if [[ "$query" == "SELECT NULL;" ]]; then
-    	results+="[null,null,null]"
+        results+="[null,null,null]"
     else
-    	results+="["
-	for i in $(seq 1 $TRIES); do
+        results+="["
+    for i in $(seq 1 $TRIES); do
             time=$(PGPASSWORD=oxla psql -h localhost -U oxla -t -c '\timing' -c "$query" | grep 'Time' | perl -nle 'm/Time: ([^ ]*) ms/; print $1 / 1000')
             echo "$time s"
             results+="$time,"
