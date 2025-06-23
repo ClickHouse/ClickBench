951beac607e3afe9fdf22a09bb35a3ec41e6e1cb
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-23 02:26:38 +0000

    Update ClickHouse tuned results

--- clickhouse/update-from-play.sql
+++ clickhouse/update-from-play.sql
@@ -49,3 +49,30 @@ WHERE suite_machine[1] = 'clickhouse' AND suite_machine[2] = 'c6a.metal'
 ORDER BY time DESC LIMIT 1
 INTO OUTFILE 'results/c6a.metal.json' TRUNCATE
 FORMAT Raw;
+
+WITH
+    extractGroups(content, '\nhttps://raw\.githubusercontent\.com/ClickHouse/ClickBench/main/([\w\-]+)/\n(\w+\.(?:\dxlarge|metal))') AS suite_machine,
+    extractAllGroups(content, '\n([\d\.]+)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)(\d+)') AS all_time_results_size,
+    all_time_results_size[1] AS regular,
+    all_time_results_size[2] AS tuned,
+    extractAll(content, '(?:Single:|Partitioned:)\n((?:\[[\d\.]+, [\d\.]+, [\d\.]+\],\n)+)') AS stateless_results
+SELECT format(
+$${{
+    "system": "ClickHouse (tuned, memory)",
+    "date": "{}",
+    "machine": "c6a.metal, 500gb gp2",
+    "cluster_size": 1,
+    "comment": "",
+    "tags": ["C++", "column-oriented", "ClickHouse derivative"],
+    "load_time": {},
+    "data_size": {},
+    "result": [
+{}
+    ]
+}}
+$$, time::Date, tuned[1], tuned[3], replaceRegexpOne(tuned[2], ',\n$', '')) AS res
+FROM sink.data
+WHERE suite_machine[1] = 'clickhouse' AND suite_machine[2] = 'c6a.metal'
+ORDER BY time DESC LIMIT 1
+INTO OUTFILE 'results/c6a.metal.tuned.memory.json' TRUNCATE
+FORMAT Raw;
