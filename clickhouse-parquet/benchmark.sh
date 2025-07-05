292061993c1458b8b6b53691881f33c280f845bb
Alexey Milovidov <milovidov@clickhouse.com>
2025-07-05 03:47:59 +0200

    Split partitioned runs to another entry

--- clickhouse-parquet/benchmark.sh
+++ clickhouse-parquet/benchmark.sh
@@ -4,11 +4,7 @@
 
 curl https://clickhouse.com/ | sh
 
-# Use for ClickHouse (Parquet, single)
-# wget --continue --progress=dot:giga 'https://datasets.clickhouse.com/hits_compatible/hits.parquet'
-
-# Use for ClickHouse (Parquet, partitioned)
-seq 0 99 | xargs -P100 -I{} bash -c 'wget --continue --progress=dot:giga https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
+wget --continue --progress=dot:giga 'https://datasets.clickhouse.com/hits_compatible/hits.parquet'
 
 # Run the queries
 
