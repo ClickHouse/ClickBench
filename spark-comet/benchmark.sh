3eb0718a63546b0d3141ef6c1e766fc8caf638aa
Iskander14yo <iskanderf.git@gmail.com>
2025-07-10 22:09:20 +0300

    add code from spark

---
+++ spark-comet/benchmark.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install
+
+sudo apt-get update -y
+sudo apt-get install -y python3-pip python3-venv openjdk-17-jdk
+
+export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-$(dpkg --print-architecture)/"
+export PATH=$JAVA_HOME/bin:$PATH
+
+python3 -m venv myenv
+source myenv/bin/activate
+pip install pyspark==4.0.0 psutil
+
+# Load the data
+
+wget --continue --progress=dot:giga 'https://datasets.clickhouse.com/hits_compatible/hits.parquet'
+
+# Run the queries
+
+./run.sh 2>&1 | tee log.txt
+
+cat log.txt | grep -P '^Time:\s+([\d\.]+)|Failure!' | sed -r -e 's/Time: //; s/^Failure!$/null/' |
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+
+echo "Data size: $(du -b hits.parquet)"
+echo "Load time: 0"
---
+++ spark-comet/query.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+import timeit
+import psutil
+import sys
+import re
+
+query = sys.stdin.read()
+# Replace \1 to $1 because spark recognizes only this pattern style (in query 28)
+query = re.sub(r"""(REGEXP_REPLACE\(.*?,\s*('[^']*')\s*,\s*)('1')""", r"\1'$1'", query)
+print(query)
+
+# Calculate available memory to configurate SparkSession
+ram = int(round(psutil.virtual_memory().available / (1024 ** 3) * 0.7))
+print(f"SparkSession will use {ram} GB of memory")
+
+spark = (
+    SparkSession
+    .builder
+    .appName("ClickBench")
+    .config("spark.driver", "local[*]") # To ensure using all cores
+    .config("spark.driver.memory", f"{ram}g") # Set amount of memory SparkSession can use
+    .config("spark.sql.parquet.binaryAsString", True) # Treat binary as string to get correct length calculations and text results
+    .getOrCreate()
+)
+
+df = spark.read.parquet("hits.parquet")
+# Do casting before creating the view so no need to change to unreadable integer dates in SQL
+df = df.withColumn("EventTime", F.col("EventTime").cast("timestamp"))
+df = df.withColumn("EventDate", F.date_add(F.lit("1970-01-01"), F.col("EventDate")))
+df.createOrReplaceTempView("hits")
+
+for try_num in range(3):
+    try:
+        start = timeit.default_timer()
+        result = spark.sql(query)
+        result.show(100) # some queries should return more than 20 rows which is the default show limit
+        end = timeit.default_timer()
+        print("Time: ", end - start)
+    except Exception as e:
+        print(e);
+        print("Failure!")
