898fe785c26587dbba921bad689efa4feaf9a92b
Iskander14yo <iskanderf.git@gmail.com>
2025-07-15 23:49:49 +0300

    auto-save results

--- spark-comet/benchmark.sh
+++ spark-comet/benchmark.sh
@@ -18,7 +18,7 @@ wget --continue --progress=dot:giga 'https://datasets.clickhouse.com/hits_compat
 
 # Install Comet
 
-wget --progress=dot:giga 'https://repo1.maven.org/maven2/org/apache/datafusion/comet-spark-spark3.5_2.12/0.9.0/comet-spark-spark3.5_2.12-0.9.0.jar' -O comet.jar
+wget --continue --progress=dot:giga 'https://repo1.maven.org/maven2/org/apache/datafusion/comet-spark-spark3.5_2.12/0.9.0/comet-spark-spark3.5_2.12-0.9.0.jar' -O comet.jar
 
 # Run the queries
 
@@ -26,6 +26,15 @@ wget --progress=dot:giga 'https://repo1.maven.org/maven2/org/apache/datafusion/c
 
 cat log.txt | grep -P '^Time:\s+([\d\.]+)|Failure!' | sed -r -e 's/Time: //; s/^Failure!$/null/' |
     awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+
+# Save results
+
+MACHINE="c6a.4xlarge"
+
+mkdir -p results
+./process_results.py "$MACHINE" > "results/${MACHINE}.json"
+
+echo "Results have been saved to results/${MACHINE}.json"
 
 echo "Data size: $(du -b hits.parquet)"
 echo "Load time: 0"
