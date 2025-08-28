0d08c39574bed1abcecf41bb8995757ae5e24c7b
Alexey Milovidov <milovidov@clickhouse.com>
2025-08-27 21:45:42 -0700

    Fix DuckDB

--- duckdb-datalake-partitioned/create.sql
+++ duckdb-datalake-partitioned/create.sql
@@ -5,6 +5,6 @@ LOAD httpfs;
 CREATE VIEW hits AS
 SELECT *
     REPLACE (make_date(EventDate) AS EventDate)
-FROM read_parquet('s3://clickhouse-public-datasets/hits_compatible/athena_partitioned/hits_*.parquet' binary_as_string=True);
+FROM read_parquet('s3://clickhouse-public-datasets/hits_compatible/athena_partitioned/hits_*.parquet', binary_as_string=True);
 
 CREATE MACRO toDateTime(t) AS epoch_ms(t * 1000);
