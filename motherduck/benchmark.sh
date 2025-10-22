d500691bd5b58838d69190dacea2800519454e8f
Elena Felder <41136058+elefeint@users.noreply.github.com>
2025-10-22 10:43:17 -0700

    remerge
    remerge

--- motherduck/benchmark.sh
+++ motherduck/benchmark.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Install
-
-sudo apt-get update -y
-sudo apt-get install -y python3-pip python3-venv
-python3 -m venv myenv
-source myenv/bin/activate
-pip install duckdb psutil
+if [ "$1" != "--skip-setup" ]; then
+  # Install python and dependencies
+  sudo apt-get update -y
+  sudo apt-get install -y python3-pip python3-venv
+  python3 -m venv myenv
+  source myenv/bin/activate
+  pip install duckdb psutil
+fi
 
 # Go to the web ui and obtain a token
-# https://motherduck.com/docs/key-tasks/authenticating-and-connecting-to-motherduck/authenticating-to-motherduck/
+# https://motherduck.com/docs/key-tasks/authenticating-and-connecting-to-motherduck/authenticating-to-motherduck/#creating-an-access-token
 # Save the token as the motherduck_token environment variable:
 # export motherduck_token=...
 
-# Load the data
-
-echo -n "Load time: "
-command time -f '%e' ./load.py
-
-# Run the queries
-
-./run.sh 2>&1 | tee log.txt
-
-cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |
-    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+python3 ./benchmark.py 2>&1
