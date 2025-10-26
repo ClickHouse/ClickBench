2a3a4bc8284303e97a65c71718bc8b83f1b68241
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-26 10:54:18 +0100

    Prevent cheating by Munich-based databases

--- hyper-parquet/query.py
+++ hyper-parquet/query.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import timeit
 import sys
+import subprocess
 
 from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode, HyperException
 
 query = sys.stdin.read()
 
-for _ in range(3):
+for try_num in range(3):
+    if try_num == 0:
+        # Flush OS page cache before first run of each query
+        subprocess.run(['sync'], check=True)
+        subprocess.run(['sudo', 'tee', '/proc/sys/vm/drop_caches'], input=b'3', check=True, stdout=subprocess.DEVNULL)
+
     start = timeit.default_timer()
     with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU) as hyper:
         with Connection(hyper.endpoint) as connection:
