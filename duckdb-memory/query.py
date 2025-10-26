58f0b8375767152bcc9ff421e6d54a28156ccbd8
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-26 10:36:18 +0100

    Timings should include connection time

--- duckdb-memory/query.py
+++ duckdb-memory/query.py
@@ -4,23 +4,25 @@ import duckdb
 import timeit
 import sys
 import os
+import subprocess
 
 with open('queries.sql', 'r') as file:
     for query in file:
-        con = duckdb.connect(':memory:')
-
-        # enable the progress bar
-        con.execute('PRAGMA enable_progress_bar;')
-        con.execute('PRAGMA enable_print_progress_bar;')
-        # disable preservation of insertion order
-        con.execute("SET preserve_insertion_order = false;")
-
         print(query)
 
         for try_num in range(3):
+            if try_num == 0:
+                # Flush OS page cache before first run of each query
+                subprocess.run(['sync'], check=True)
+                subprocess.run(['sudo', 'tee', '/proc/sys/vm/drop_caches'], input=b'3', check=True, stdout=subprocess.DEVNULL)
+
             start = timeit.default_timer()
+            con = duckdb.connect(':memory:')
 
             if try_num == 0:
+                # disable preservation of insertion order
+                con.execute("SET preserve_insertion_order = false;")
+
                 con.execute(open("create.sql").read())
                 con.execute(open("load.sql").read())
                 print("Load time: {}", round(timeit.default_timer() - start, 3))
