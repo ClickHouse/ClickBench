7d7974d36dfe2c255bc1f1d5526bc77fd33ad792
Caetano Sauer <caetanosauer@gmail.com>
2025-11-11 23:56:16 +0100

    Fix Hyper scripts to perform hot runs
    A recent commit aimed at "Prevent cheating by Munich-based databases"
    (4a2f3fa) incorrectly modified Hyper's `query.py` scripts to restart the
    database server on each iteration, essentially making every run a cold
    run. This unfairly brought our results down by a large margin.
    
    This commit reverts the previous behavior which was already correct and
    there was absolutely no cheating involved.

--- hyper-parquet/query.py
+++ hyper-parquet/query.py
@@ -7,17 +7,17 @@ from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode, Hyp
 
 query = sys.stdin.read()
 
-for try_num in range(3):
-    if try_num == 0:
-        # Flush OS page cache before first run of each query
-        subprocess.run(['sync'], check=True)
-        subprocess.run(['sudo', 'tee', '/proc/sys/vm/drop_caches'], input=b'3', check=True, stdout=subprocess.DEVNULL)
+with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU) as hyper:
+    with Connection(hyper.endpoint) as connection:
+        # Hyper only supports temporary external tables, so we need to create them on every query
+        connection.execute_command(open("create.sql").read())
+        for try_num in range(3):
+            if try_num == 0:
+                # Flush OS page cache before first run of each query
+                subprocess.run(['sync'], check=True)
+                subprocess.run(['sudo', 'tee', '/proc/sys/vm/drop_caches'], input=b'3', check=True, stdout=subprocess.DEVNULL)
 
-    start = timeit.default_timer()
-    with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU) as hyper:
-        with Connection(hyper.endpoint) as connection:
-            # Hyper only supports temporary external tables, so we need to create them on every query
-            connection.execute_command(open("create.sql").read())
+            start = timeit.default_timer()
             try:
                 connection.execute_list_query(query)
                 print(round(timeit.default_timer() - start, 3))
