d9a752b1c49c94f6f120bc423557d489e443df01
Alexey Milovidov <milovidov@clickhouse.com>
2025-12-13 17:44:57 -0800

    Correct storage sizes for data frames when they use swap

--- chdb-dataframe/benchmark.sh
+++ chdb-dataframe/benchmark.sh
@@ -20,7 +20,11 @@ wget --continue --progress=dot:giga https://datasets.clickhouse.com/hits_compati
 
 # Run the queries
 
+SWAP_BEFORE=$(free | awk '/Swap/{print $3}')
 /usr/bin/time -f "Memory usage: %M KB" ./query.py 2>&1 | tee log.txt
+SWAP_AFTER=$(free | awk '/Swap/{print $3}')
 
-echo -n "Data size: "
-grep -F "Memory usage" log.txt | grep -o -P '\d+ KB' | sed 's/KB/*1024/' | bc -l
+MEM=$(grep -F "Memory usage" log.txt | grep -o -P '\d+')
+SWAP_DELTA=$((SWAP_AFTER - SWAP_BEFORE))
+SWAP_DELTA=$((SWAP_DELTA > 0 ? SWAP_DELTA : 0))
+echo "Data size: $(( (MEM + SWAP_DELTA) * 1024 ))"
