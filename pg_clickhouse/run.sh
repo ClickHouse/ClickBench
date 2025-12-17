454e65e9616e8b7f89527958058d544ff3032145
David E. Wheeler <david.wheeler@clickhouse.com>
2025-12-17 17:28:50 -0500

    Add pg_clickhouse
    It dupes the clickhouse benchmark config to load data and the Postgres
    config to run the queries. All queries push down to ClickHouse without
    error.

---
+++ pg_clickhouse/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TRIES=3
+
+cat queries.sql | while read -r query; do
+    sync
+    echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+    echo "$query"
+    (
+        echo '\timing'
+        yes "$query" | head -n $TRIES
+    ) | sudo -u postgres psql --no-psqlrc --tuples-only test -t 2>&1 | grep -P 'Time|psql: error' | tail -n1
+done
