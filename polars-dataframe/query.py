5c5204c8b641b58bb54bc5ed14610f7f2bac3be0
ritchie46 <ritchie46@gmail.com>
2025-12-25 13:37:05 +0100

    Use Polars streaming

--- polars-dataframe/query.py
+++ polars-dataframe/query.py
@@ -6,6 +6,8 @@ from datetime import date
 import json
 import os
 
+pl.Config.set_engine_affinity("streaming")
+
 # 0: No., 1: SQL, 2: Polars
 queries = [
     ("Q0", "SELECT COUNT(*) FROM hits;", lambda x: x.select(pl.len()).collect().height),
