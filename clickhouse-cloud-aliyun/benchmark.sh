9bbc29199f951e54375563a4a32f5a140be17061
愉慜 <muxiaobin.mxb@alibaba-inc.com>
2026-01-13 21:11:03 +0800

    Add AliCloud ClickBench Results

---
+++ clickhouse-cloud-aliyun/benchmark.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+# Load the data
+
+# export FQDN=...
+# export USER=...
+# export PASSWORD=...
+# export AK=...
+# export SK=...
+# export STORAGE=...
+# export REPLICAS=...
+# export CCU=...
+
+url="https://clickhouse-test-clickbench-hangzhou.oss-cn-hangzhou-internal.aliyuncs.com/clickbench/hits_parquets/hits_{0..99}.parquet"
+ 
+export CATEGORY="ent"
+ 
+MAX_INSERT_THREADS=$(clickhouse-client --host "$FDQN" --user "$USER" --password "$PASSWORD" --query "SELECT intDiv(getSetting('max_threads'), 4)")
+
+load_time=$(clickhouse-client --host "$FDQN" --user "$USER" --password "$PASSWORD" --enable-parallel-replicas 1 --max-insert-threads $MAX_INSERT_THREADS --query="INSERT INTO hits SELECT * FROM s3Cluster('default', '$url', '$AK', '$SK', 'parquet');" --time 2>&1)
+
+
+result=$(./run.sh)
+
+data_size=$(clickhouse-client --host "$FDQN" --user "$USER" --password "$PASSWORD" --query="SELECT total_bytes FROM system.tables WHERE name = 'hits' AND database = 'default'")
+
+echo '
+{
+    "system": "ClickHouse ☁️ (alicloud-'$STORAGE')",
+    "date": "'$(date +%F)'",
+    "machine": "AliCloud: '$CCU'CCU",
+    "cluster_size": '$REPLICAS',
+
+    "proprietary": "yes",
+    "hardware": "cpu",
+    "tuned": "no",
+    "comment": "",
+
+    "tags": ["C++", "column-oriented", "ClickHouse derivative", "managed", "alicloud"],
+
+    "load_time": '$load_time',
+    "data_size": '$data_size',
+
+    "result": [
+      '$(echo "$result" | sed '$ s/.$//')'
+    ]
+}
+' > "results/alicloud-$CATEGORY-$REPLICAS-$STORAGE-$CCU-$REPLICAS.json"
---
+++ clickhouse-cloud-aliyun/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+TRIES=3
+QUERY_NUM=1
+cat queries.sql | while read -r query; do
+    echo -n "["
+    for i in $(seq 1 $TRIES); do
+      CACHE_PARAMS=''
+      if [[ "$i" == 1 ]];then
+        CACHE_PARAMS=',enable_filesystem_cache=0'
+      fi
+      (clickhouse-client --time --format=Null --query="$query settings log_queries=1$CACHE_PARAMS;" --progress 0 2>&1 |
+          grep -o -E '^[0-9]+\.[0-9]+$' || echo -n "null") | tr -d '\n'
+ 
+      [[ "$i" != $TRIES ]] && echo -n ", "
+    done
+    echo "],"
+ 
+    QUERY_NUM=$((QUERY_NUM + 1))
+done
