85129bd1b8eee847c85d7c4a1f451b285544eea3
Ruihang Xia <waynestxia@gmail.com>
2026-01-15 19:09:48 +0800

    add swap to build command
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- datafusion/benchmark.sh
+++ datafusion/benchmark.sh
@@ -14,7 +14,9 @@ echo "Install DataFusion main branch"
 git clone https://github.com/apache/datafusion.git
 cd datafusion/
 git checkout 52.0.0
+sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
 CARGO_PROFILE_RELEASE_LTO=true RUSTFLAGS="-C codegen-units=1" cargo build --release --package datafusion-cli --bin datafusion-cli
+sudo swapoff /swapfile
 export PATH="`pwd`/target/release:$PATH"
 cd ..
 
