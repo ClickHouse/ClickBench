a575e030b6db4e40fc814ad031cd5cac6afed5a3
Ruihang Xia <waynestxia@gmail.com>
2026-01-23 17:41:01 +0800

    use prebuilt binary instead
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- datafusion/benchmark.sh
+++ datafusion/benchmark.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo "Install Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust-init.sh
-bash rust-init.sh -y
-export HOME=${HOME:=~}
-source ~/.cargo/env
-
 echo "Install Dependencies"
 sudo apt-get update -y
-sudo apt-get install -y gcc
 
-echo "Install DataFusion main branch"
-git clone https://github.com/apache/datafusion.git
-cd datafusion/
-git checkout 52.0.0
-sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
-CARGO_PROFILE_RELEASE_LTO=true RUSTFLAGS="-C codegen-units=1" cargo build --release --package datafusion-cli --bin datafusion-cli
-sudo swapoff /swapfile
-export PATH="`pwd`/target/release:$PATH"
-cd ..
+echo "Install Homebrew"
+# This requires password input for sudo, which is not set by default.
+# You may need to run the following command to set a password first:
+# ```
+# sudo su
+# passwd ubuntu
+# exit
+# ```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /home/ubuntu/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> /home/ubuntu/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+
+echo "Install datafusion-cli"
+# or use `brew install datafusion@52` to install a specific version
+brew install datafusion
+datafusion-cli --version
 
 echo "Download benchmark target data, single file"
 wget --continue --progress=dot:giga https://datasets.clickhouse.com/hits_compatible/hits.parquet
