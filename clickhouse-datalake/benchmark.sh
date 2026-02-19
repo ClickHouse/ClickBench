5b1e1c01b35d37f238603b14fb52a7e759863b1d
Robert Schulze <robert@clickhouse.com>
2026-02-19 00:30:38 +0000

    Work around https://github.com/ClickHouse/ClickHouse/issues/97325

--- clickhouse-datalake/benchmark.sh
+++ clickhouse-datalake/benchmark.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Install
-
 curl https://clickhouse.com/ | sh
-
-# Configure
-
-> clickhouse-local.yaml echo "
-filesystem_caches:
-    cache:
-        path: '/dev/shm/clickhouse/'
-        max_size_ratio_to_total_space: 0.9
-"
-
-# Run the queries
 
 ./run.sh
 
